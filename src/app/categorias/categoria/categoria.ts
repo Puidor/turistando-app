31852fecc8c02b293d471e78646e4a70e5489243
Carlos Adriano <c.adriano.silva@hotmail.com>
2025-08-16 17:02:10 -0300

    wip: criando categoria model e incrementando component com validações

--- src/app/categorias/categoria/categoria.ts
+++ src/app/categorias/categoria/categoria.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-categoria',
   standalone: false,
   templateUrl: './categoria.html',
-  styleUrl: './categoria.scss'
+  styleUrl: './categoria.scss',
 })
 export class Categoria {
+  camposForm: FormGroup;
 
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      descricao: new FormControl('', Validators.required),
+    });
+  }
+
+  salvar() {
+    this.camposForm.markAllAsTouched();
+    if (this.camposForm.valid) {
+      console.log('Form Submitted', this.camposForm.value);
+      console.log('Está válido?', this.camposForm.valid);
+    }
+  }
+
+  isCampoIvalido(nomeCampo: string): boolean {
+    const campo = this.camposForm.get(nomeCampo);
+    return (campo?.invalid && campo.touched) || false;
+  }
 }
--- src/app/categorias/categorias-module.ts
+++ src/app/categorias/categorias-module.ts
@@ -1,17 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { CategoriasRoutingModule } from './categorias-routing-module';
 import { Categoria } from './categoria/categoria';
 
-
 @NgModule({
-  declarations: [
-    Categoria
-  ],
-  imports: [
-    CommonModule,
-    CategoriasRoutingModule
-  ]
+  declarations: [Categoria],
+  imports: [CommonModule, CategoriasRoutingModule, ReactiveFormsModule],
 })
-export class CategoriasModule { }
+export class CategoriasModule {}
